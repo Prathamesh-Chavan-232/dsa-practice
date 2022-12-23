f5e1056a1ff8fcb066fa9c17e41e4bc8363bf72d
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-12-23 14:57:11 +0530

    Rearranged

---
+++ Algorithms/path_finding/09_5_A_star.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortening syntax
+#define ll long long
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+// Shortenting stl function calls
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+
+// Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef priority_queue<int> pqb;
+typedef priority_queue<int, vector<int>, greater<int>> pqs;
+
+// Container input tools
+void inVec(vector<int> &v) {
+    string s;
+    getline(cin, s);
+    int num = 0, sign = 1;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '[' || s[i] == ']')
+            continue;
+        if (s[i] == '-')
+            sign = -1;
+        else if (s[i] == ',' || s[i] == ' ')
+            v.push_back(num), num = 0;
+        else {
+            num = num * 10 + (s[i] - '0');
+            num *= sign;
+            sign = 1;
+        }
+    }
+    v.push_back(num);
+}
+
+// Varidiac Variable debugger
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+template <typename... Args>
+void logger(string varname, Args &&...values)  // logger for varadiac debugging print statements
+{
+    cerr << varname << " =";
+    string delim = " ";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+
+// debugger for STL vector / set (of any type)
+#ifndef ONLINE_JUDGE
+#define debcon(x)        \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << "\n";
+#else
+#define debcon(x)
+#endif
+template <typename T>
+void _print(T const &c) {
+    cerr << "{ ";
+    foreach (it, c) {
+        it != --c.end() ? cerr << *it << ", " : cerr << *it;
+    }
+    cerr << "}";
+}
+
+// constants
+const int mod = 1e9 + 7;
+const int N = 1e5 + 10, M = N;
+const double PI = 3.1415926535897932384626;
+
+/**
+ * @brief-
+ *
+ *
+ */
+
+// Global variables
+vi graph[N];
+bool vis[N];
+
+// classes & functions
+void a_star() {
+    // Path finding search
+}
+
+void code() {
+    // cout << res << endl;
+}
+
+int main() {
+    // Start time
+    auto start = chrono::steady_clock::now();
+
+    // fast io
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+// Input, Output & error messages inside text files
+#ifndef ONLINE_JUDGE
+    freopen("/home/falconcodes/prathamesh/programming/input.txt", "r", stdin);
+    freopen("/home/falconcodes/prathamesh/programming/output.txt", "w", stdout);
+    freopen("/home/falconcodes/prathamesh/programming/err.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        code();
+    }
+
+// Calculating Runtime
+#ifndef ONLINE_JUDGE
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cerr << "[Finished in " << setprecision(3) << chrono::duration<double, milli>(diff).count() << " ms]\n";
+#endif
+    return 0;
+}
--- Trees/10_1_BST.cpp
+++ Trees/10_1_BST.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
 #include <bits/stdc++.h>
+
+#include <iostream>
 using namespace std;
 
-class node
-{
+class node {
     int data;
     node *left;
     node *right;
 
-public:
+   public:
     node() {}
-    node(int data)
-    {
+    node(int data) {
         this->data = data;
         left = NULL;
         right = NULL;
@@ -26,24 +25,20 @@ public:
     friend node *inorderPre(node *root);
 };
 
-void create_tree(node *&root)
-{
+void create_tree(node *&root) {
     char isleft = 'n', isright = 'n';
     int data;
     cin >> data >> isleft >> isright;
     root = new node(data);
-    if (isleft == 'y' || isleft == 'Y')
-    {
+    if (isleft == 'y' || isleft == 'Y') {
         create_tree(root->left);
     }
-    if (isright == 'y' || isright == 'Y')
-    {
+    if (isright == 'y' || isright == 'Y') {
         create_tree(root->right);
     }
 }
 
-void inorder(node *&root)
-{
+void inorder(node *&root) {
     if (root == NULL)
         return;
     inorder(root->left);
@@ -51,151 +46,109 @@ void inorder(node *&root)
     inorder(root->right);
 }
 
-bool isBST(node *&root)
-{
+bool isBST(node *&root) {
     static node *prev = NULL;
-    if (root != NULL)
-    {
-        if (!isBST(root->left))
-        {
+    if (root != NULL) {
+        if (!isBST(root->left)) {
             return false;
         }
-        if (prev != NULL && root->data <= prev->data)
-        {
+        if (prev != NULL && root->data <= prev->data) {
             return false;
         }
         prev = root;
         return isBST(root->right);
-    }
-    else
-    {
+    } else {
         return true;
     }
 }
 
-node *search_BST(node *&root, int key)
-{
-
-    if (root == NULL)
-    {
+node *search_BST(node *&root, int key) {
+    if (root == NULL) {
         return NULL;
     }
-    if (key == root->data)
-    {
+    if (key == root->data) {
         return root;
-    }
-    else if (key < root->data)
-    {
+    } else if (key < root->data) {
         return search_BST(root->left, key);
-    }
-    else
-    {
+    } else {
         return search_BST(root->right, key);
     }
 }
 
-void insert_BST(node *&root, node *&prev, int key)
-{
-
-    if (root == NULL)
-    {
+void insert_BST(node *&root, node *&prev, int key) {
+    if (root == NULL) {
         node *temp = new node(key);
-        if (prev == NULL)
-        {
+        if (prev == NULL) {
             root = temp;
         }
-        if (key < prev->data)
-        {
-
+        if (key < prev->data) {
             prev->left = temp;
-        }
-        else
-        {
+        } else {
             prev->right = temp;
         }
         cout << "node inserted\n";
         return;
     }
-    if (key == root->data)
-    {
+    if (key == root->data) {
         cout << "Already exists\n";
         return;
-    }
-    else if (key < root->data)
-    {
+    } else if (key < root->data) {
         insert_BST(root->left, root, key);
-    }
-    else
-    {
+    } else {
         insert_BST(root->right, root, key);
     }
 }
 
-node *insert(node *root, int key)
-{
-    if (root == NULL)
-    {
+node *insert(node *root, int key) {
+    if (root == NULL) {
         return new node(key);
     }
-    if (key < root->data)
-    {
+    if (key < root->data) {
         root->left = insert(root->left, key);
-    }
-    else if (key > root->data)
-    {
+    } else if (key > root->data) {
         root->right = insert(root->right, key);
     }
     return root;
 }
 
-node *inorderPre(node *root)
-{
+node *inorderPre(node *root) {
     root = root->left;
-    while (root->right != NULL)
-    {
+    while (root->right != NULL) {
         root = root->right;
     }
     return root;
 }
-node *delete_BST(node *root, int key)
-{
+node *delete_BST(node *root, int key) {
     node *prev;
-    if (root == NULL)
-    {
+    if (root == NULL) {
         return NULL;
     }
-    if (root->left == NULL && root->right)
-    {
+    if (root->left == NULL && root->right) {
         free(root);
         return NULL;
     }
 
     // Search for the node to be deleted
-    if (key < root->data)
-    {
+    if (key < root->data) {
         root->left = delete_BST(root->left, key);
-    }
-    else if (key > root->data)
-    {
+    } else if (key > root->data) {
         root->right = delete_BST(root->right, key);
     }
 
     // Deletion stratergy
-    else
-    {
+    else {
         prev = inorderPre(root);
         root->data = prev->data;
         root->left = delete_BST(root->left, prev->data);
     }
     return root;
 }
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 #ifndef ONLINE_JUDGE
-    freopen("C:/Prathamesh/Programming/input.txt", "r", stdin);   // input
-    freopen("C:/Prathamesh/Programming/output.txt", "w", stdout); // output
+    freopen("C:/Prathamesh/Programming/input.txt", "r", stdin);    // input
+    freopen("C:/Prathamesh/Programming/output.txt", "w", stdout);  // output
 #endif
     node *root;
     create_tree(root);
